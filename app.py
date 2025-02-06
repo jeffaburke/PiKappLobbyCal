4c1dc3a3737c446a22cb895ce5dd46fb7bb6db47
JeffABurke <jburke0519@gmail.com>
2025-02-05 19:24:31 -0500

    Unused import

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
